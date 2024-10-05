e96d9093216ddf5a073a6bee847717a77dcba55e
datle-gh <datle-gh>
2024-10-05 13:43:21 +0700

    1st version

---
+++ Arduino/Arduino.ino
@@ -0,0 +1,171 @@
+//Library
+#include <Arduino.h>
+#include <HX711.h>
+#include <Servo.h>
+#include <SoftwareSerial.h>
+
+//define pin, variable, constraint
+
+//Setup pin to communicate with esp by UART
+#define RX_PIN 0 // RX pin for Software Serial
+#define TX_PIN 1 // TX pin for Software Serial
+SoftwareSerial espSerial(RX_PIN, TX_PIN); // RX = 0, TX = 1
+
+  //load cell
+const int LOADCELL_DOUT_PIN = A0;
+const int LOADCELL_SCK_PIN = A1;
+HX711 scale;
+
+  //food door: servo motor
+Servo foodDoor;  //servo object
+const int foodDoorPin = 9;
+const int weightLowestVal = 100;   //100g
+const int weightHighestVal = 300;  //300g
+float weightCurentVal = 0;
+bool isDoorOpen = false;
+char weightLevelVal[50];
+
+  //led
+const int ledPin =  4;
+int ledState = LOW;
+
+  //button
+const int buttonPin =  7;
+int buttonState = 0;
+
+  //time set
+int feedingHour = 0;
+int feedingMinute = 0;
+
+  //message between esp and arduino
+char message[100]
+
+//--------------------SET UP----------------------------
+
+void setup() {
+  //button 
+  pinMode(buttonPin, INPUT);  
+
+  //led
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, ledState);
+
+  //servo set up
+  foodDoor.attach(foodDoorPin);
+
+  Serial.println("Initializing the scale");
+  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+  scale.set_scale(-533.6609523810);  // this value is obtained by calibrating the scale with known weights; see the README for details
+  scale.tare();                      // reset the scale to 0
+
+  Serial.begin(9600);
+  espSerial.begin(9600);
+}
+
+//-------------------------METHOD------------------------
+
+//turn on the led
+void turnLedOn(){
+  if(ledState == LOW){
+    ledState = HIGH;
+  }
+  digitalWrite(ledPin, ledState);
+}
+
+//turn off the led
+void turnLedOff(){
+  if(ledState == HIGH){
+    ledState = LOW;
+  }
+  digitalWrite(ledPin, ledState);
+}
+
+//rotate servo to open
+void openFoodDoor() {
+  if(isDoorOpen == false){
+    foodDoor.write(100);  // Rotate to 100 degree
+    isDoorOpen = true;
+  }
+}
+
+//rotate servo to close
+void closeFoodDoor() {
+  if(isDoorOpen == true){
+    foodDoor.write(0);  // Rotate to 0 degree
+    isDoorOpen = false;
+  }
+}
+
+//check if the food amount has reached the maximum weight
+bool checkEnoughFood(){
+  bool isEnough = false;
+  if(weightCurentVal >= weightHighestVal)
+    isEnough = true;
+  return isEnough;
+}
+
+//check if the feeding time has been set
+bool checkTimeSet(){
+  bool isTimeSet = false;
+  if (espSerial.available()) {
+    String command = espSerial.readStringUntil('\n');
+
+    if (command == "FEED") {
+      Serial.println("Feeding the pet...");
+      isTimeSet = true;
+    }
+  }
+  return isTimeSet;
+}
+
+//read data from load cell sensor
+void readScale() {
+  Serial.println("After setting up the scale:");
+  weightCurentVal = scale.get_units();
+}
+
+//read the button state
+void readButton(){
+  buttonState = digitalRead(buttonPin);
+}
+
+//send data to esp
+void sendESP(){
+  dtostrf((float)33.3333, 10, 9, weightLevelVal);  // (float value, min width, precision, buffer)
+  // Send data to ESP
+  message[0] = '\0';
+  strcat(message, weightLevelVal);
+  espSerial.print(message); /*send string on request */
+}
+
+//-----------------------------LOOP-------------------------------------
+
+void loop() {
+  //Receive data from esp: time set
+  bool isTimeReached = checkTimeSet();
+
+  // Read load sensor value
+  readScale();  // Read the weight value
+  Serial.print("Food weight (g): ");
+  Serial.println(weightCurentVal);  // Print the food weight value to Serial Monitor
+  // Read button state
+  readButton();
+
+  // send data to esp
+  sendESP();
+
+  //check button state and feeding time to dispense food
+  if ( buttonState || isTimeReached) {
+    do {
+      turnLedOn();
+      openFoodDoor();
+      readScale();
+    } while (checkEnoughFood());
+  }
+  turnLedOff();
+  closeFoodDoor();
+  sendESP();
+
+  //delay 1 minute
+  delay(1000);
+}
---
+++ ESP8266/ESP8266.ino
@@ -0,0 +1,166 @@
+//define pin with GPIO 
+static const uint8_t D0 = 16;
+static const uint8_t D1 = 5;
+static const uint8_t D2 = 4;
+static const uint8_t D3 = 0;
+static const uint8_t D4 = 2;
+static const uint8_t D5 = 14;
+static const uint8_t D6 = 12;
+static const uint8_t D7 = 13;
+static const uint8_t D8 = 15;
+static const uint8_t D9 = 3;
+static const uint8_t D10 = 1;
+
+//define blynk token
+#define BLYNK_TEMPLATE_ID "TMPL6zLs-GJfK"
+#define BLYNK_TEMPLATE_NAME "Pet Feeder"
+#define BLYNK_AUTH_TOKEN "iL0Bwv9Lgm39QtufdqnMf9QVuRgJuYIV"
+
+#define BLYNK_PRINT Serial
+
+//include library
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+#include <SoftwareSerial.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+//wifi credential
+char ssid[] = ""; //Wifi name
+char pass[] = ""; //Wifi password
+
+//Blynk: Virtual timer
+#define VIRTUAL_PIN_HOUR_INPUT V1
+#define VIRTUAL_PIN_MINUTE_INPUT V2
+#define VIRTUAL_PIN_WEIGHT_OUTPUT V3
+
+//Blynk: Set up Real time on blynk
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "asia.pool.ntp.org", 7 * 3600, 60000);
+
+//Blynk: creates the timer object. 
+//allows you to send data periodically. 
+//prevents spamming the Blynk.Cloud with thousands of messages from your hardware
+BlynkTimer timer;
+
+//Set up pin to communicate with Arduino by UART
+#define RX_PIN D9 
+#define TX_PIN D10 
+SoftwareSerial arduinoSerial(RX_PIN, TX_PIN);
+
+// Declare LCD (I2C address, columns, rows)
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+#define SDA_PIN D2
+#define SCL_PIN D1
+
+//define variable
+float weightLevelVal;
+String message;
+
+// variable to store feeding time
+int feedingHour = 0;
+int feedingMinute = 0;
+// variable to store the current time
+int currentHour = 0;
+int currentMinute = 0;
+
+//-------------------SET UP-------------------------------------------------
+
+void setup() {
+  //debug console
+  Serial.begin(9600);
+  //set up to communicate with ESP
+  arduinoSerial.begin(9600);
+
+  //set up Blynk
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
+
+  // Initialize NTP Client
+  timeClient.begin();
+
+  //set up a function to be called at any time
+  //  5000 milliseconds (5 seconds), and the letter L is used to specify that this value is a long integer
+  timer.setInterval(5000L , myTimerEvent);
+
+  //set up lcd
+  Wire.begin(SDA_PIN, SCL_PIN); //set up I2C pins
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("Waiting for time");
+}
+
+//--------------------METHOD--------------------------------------
+
+// Event handler from Blynk: Get Hour input
+//Input: virtual pin V1 (on Blynk)
+BLYNK_WRITE(VIRTUAL_PIN_HOUR_INPUT) {
+  feedingHour = param.asInt();  // Get value from Slider 1 (hour)
+  Serial.print("Selected Hour: ");
+  Serial.println(feedingHour);
+}
+
+// Event handler from Blynk: Get Minute input
+//Input: virtual pin V2 (on Blynk)
+BLYNK_WRITE(VIRTUAL_PIN_MINUTE_INPUT){
+  feedingMinute = param.asInt();  // Get value from Slider 2 (minute)
+  Serial.print("Selected Minute: ");
+  Serial.println(feedingMinute);
+}
+
+//call method after a time period based on timer.setInterval, ignore loop()
+void myTimerEvent(){
+  getRealTime();
+  sendBlynkWeight();
+  displayTimeSet();
+}
+
+//Receive data from Arduino and send the weight value to Blynk
+void sendBlynkWeight(){
+  message = "";
+  Serial.println(message);
+  message = arduinoSerial.read();
+  weightLevelVal = message.toFloat(); 
+  Blynk.virtualWrite(VIRTUAL_PIN_WEIGHT_OUTPUT, weightLevelVal);
+}
+
+// Get current time through Blynk
+void getRealTime(){
+  timeClient.update();
+  currentHour = timeClient.getHours();
+  currentMinute = timeClient.getMinutes();
+}
+
+// Display the time on LCD
+void displayTimeSet(){
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Feed Time Set:");
+    lcd.setCursor(0, 1);
+    lcd.print(feedingHour);
+    lcd.print(":");
+    if (feedingMinute < 10) {
+      lcd.print("0");  // Add leading zero if minute < 10
+    }
+    lcd.print(feedingMinute);
+}
+
+//------------------------LOOP----------------------------------------------
+
+void loop() {
+  // Run Blynk
+  Blynk.run();
+  //run timer to send data periodically
+  timer.run();
+
+  //send signal to feed
+  if (currentHour == feedingHour && currentMinute == feedingMinute) {
+    Serial.println("Feeding the pet...");
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Feeding now!");
+    arduinoSerial.println("FEED");
+  }
+}
