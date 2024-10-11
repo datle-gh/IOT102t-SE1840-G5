74d461823f84874ddb14d34f8d8238e41a332d01
datle-gh <datle-gh>
2024-10-11 17:28:56 +0700

    Update V4 19/10/2024

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -1,30 +1,35 @@
+#include <Wire.h>
+// #include <SoftI2C.h>
 //Library
 #include <Arduino.h>
 #include <HX711.h>
 #include <Servo.h>
-#include <SoftwareSerial.h>
+// #include <SoftwareSerial.h>
 
 //define pin, variable, constraint
 
-//Setup pin to communicate with esp by UART
-#define RX_PIN 10//2 0 // RX pin for Software Serial
-#define TX_PIN 11//3 1 // TX pin for Software Serial
-SoftwareSerial espSerial(RX_PIN, TX_PIN); // RX = 0, TX = 1
+// //Setup pin to communicate with esp by UART
+// #define RX_PIN 10//2 0 // RX pin for Software Serial
+// #define TX_PIN 11//3 1 // TX pin for Software Serial
+// SoftwareSerial espSerial(RX_PIN, TX_PIN); // RX = 0, TX = 1
+
+#define ESP8266_I2C_ADDRESS 8
 
   //load cell
 const int LOADCELL_DOUT_PIN = A0;
 const int LOADCELL_SCK_PIN = A1;
 HX711 scale;
-const float CALIBRATING = 0;// this value is obtained by calibrating the scale with known weights; see the README for details
+const float CALIBRATING = 412.234;// this value is obtained by calibrating the scale with known weights; see the README for details
 
   //food door: servo motor
 Servo foodDoor;  //servo object
 const int foodDoorPin = 9;
 const int weightLowestVal = 100;   //100g
 const int weightHighestVal = 300;  //300g
-float weightCurentVal = 0;
+float weightCurrentVal = 0;
 bool isDoorOpen = false;
-char weightLevelVal[50];
+
+bool isTimeReached;
 
   //led
 const int ledPin =  4;
@@ -32,11 +37,7 @@ int ledState = LOW;
 
   //button
 const int buttonPin =  7;
-int buttonState = 0;
-
-  //time set
-int feedingHour = 0;
-int feedingMinute = 0;
+int buttonState = LOW;
 
   //message between esp and arduino
 char message[100];
@@ -53,6 +54,7 @@ void setup() {
 
   //servo set up
   foodDoor.attach(foodDoorPin);
+  foodDoor.write(1); 
 
   Serial.println("Initializing the scale");
   scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
@@ -60,10 +62,18 @@ void setup() {
   scale.tare();                      // reset the scale to 0
 
   Serial.begin(9600);
-  espSerial.begin(9600);
+  // espSerial.begin(9600);
+  Wire.begin(ESP8266_I2C_ADDRESS);
+  Wire.onReceive(receiveEvent);
+  Wire.onRequest(requestEvent);
 }
 
 //-------------------------METHOD------------------------
+
+//read the button state
+void readButton(){
+  buttonState = digitalRead(buttonPin);
+}
 
 //turn on the led
 void turnLedOn(){
@@ -84,7 +94,7 @@ void turnLedOff(){
 //rotate servo to open
 void openFoodDoor() {
   if(isDoorOpen == false){
-    foodDoor.write(100);  // Rotate to 100 degree
+    foodDoor.write(120);  // Rotate to 100 degree
     isDoorOpen = true;
   }
 }
@@ -100,73 +110,94 @@ void closeFoodDoor() {
 //check if the food amount has reached the maximum weight
 bool checkEnoughFood(){
   bool isEnough = false;
-  if(weightCurentVal >= weightHighestVal)
+  if(weightCurrentVal >= weightHighestVal){
     isEnough = true;
+  }
   return isEnough;
 }
 
 //check if the feeding time has been set
-bool checkTimeSet(){
-  bool isTimeSet = false;
-  if (espSerial.available()) {
-    String command = espSerial.readStringUntil('\n');
-
-    if (command == "FEED") {
-      Serial.println("Feeding the pet...");
-      isTimeSet = true;
-    }
-  }
-  return isTimeSet;
-}
+// bool checkTimeSet(){
+//   bool isTimeSet = false;
+//   if (espSerial.available()) {
+//     String command = espSerial.readStringUntil("\n");
+//     Serial.println(command);
+//     if (command == "FEED") {
+//       Serial.println("Feeding the pet...");
+//       isTimeSet = true;
+//     }
+//   }
+//   return isTimeSet;
+// }
 
 //read data from load cell sensor
 void readScale() {
   Serial.println("After setting up the scale:");
-  weightCurentVal = scale.get_units(10);
-}
-
-//read the button state
-void readButton(){
-  buttonState = digitalRead(buttonPin);
+  weightCurrentVal = scale.get_units();
+  weightCurrentVal = weightCurrentVal - 22; //box's weight = 22g
+  if(weightCurrentVal <=0){
+    weightCurrentVal = 0;
+  }
+  Serial.print("Food weight (g): ");
+  Serial.println(weightCurrentVal);  // Print the food weight value to Serial Monitor
 }
 
 //send data to esp
-void sendESP(){
-  dtostrf((float)33.3333, 10, 9, weightLevelVal);  // (float value, min width, precision, buffer)
-  // Send data to ESP
-  message[0] = '\0';
-  strcat(message, weightLevelVal);
-  espSerial.print(message); /*send string on request */
+// void sendESP(){
+//   char weightString[10]; 
+//   dtostrf((float)weightCurentVal, 3, 2, weightString);  // (float value, min width, precision, buffer)
+//   // Serial.println(weightString);
+//   // Send data to ESP
+//   message[0] = '\0';
+//   strcat(message, weightString);
+//   Serial.println("Send esp");
+//   espSerial.println(message);
+// }
+
+// Hàm gửi dữ liệu cân nặng cho ESP8266 khi có yêu cầu
+void requestEvent() {
+  Wire.write((byte*)&weightCurrentVal, sizeof(weightCurrentVal));
+}
+
+// Hàm nhận tín hiệu từ ESP8266 (Master) để cho ăn
+void receiveEvent(int bytes) {
+  if (bytes == 1) {
+    byte signal = Wire.read();
+    if (signal == 1) {
+      Serial.println("Time to feed the pet!");
+      isTimeReached = true;
+    }
+  }
 }
 
 //-----------------------------LOOP-------------------------------------
 
 void loop() {
   //Receive data from esp: time set
-  bool isTimeReached = checkTimeSet();
+  // bool isTimeReached = checkFeedingTimeSignal();
 
   // Read load sensor value
   readScale();  // Read the weight value
-  Serial.print("Food weight (g): ");
-  Serial.println(weightCurentVal);  // Print the food weight value to Serial Monitor
-  // Read button state
   readButton();
+  if(buttonState == HIGH){
+    Serial.println("Button pressed!");
+  }
 
   // send data to esp
-  sendESP();
-
+  // sendESP();
   //check button state and feeding time to dispense food
-  if ( buttonState || isTimeReached) {
+  if ( (buttonState == HIGH) || isTimeReached) {
     do {
       turnLedOn();
       openFoodDoor();
       readScale();
-    } while (checkEnoughFood());
+      delay(800);
+    } while (!checkEnoughFood());
+    isTimeReached = false;
   }
   turnLedOff();
   closeFoodDoor();
-  sendESP();
-
+  // sendESP();
   //delay 1 minute
-  delay(1000);
+  delay(800);
 }
--- ESP8266/ESP8266.ino
+++ ESP8266/ESP8266.ino
@@ -8,15 +8,15 @@
 //include library
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
-#include <SoftwareSerial.h>
+// #include <SoftwareSerial.h>
 #include <NTPClient.h>
 #include <WiFiUdp.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
 //wifi credential
-char ssid[] = "FPTU_Library"; //Wifi name
-char pass[] = "12345678"; //Wifi password
+char ssid[] = "Bich Tram"; //Wifi name :FPTU_Library     FPt-Student     Bích Trâm
+char pass[] = "18042004"; //Wifi password         18042004
 
 //Blynk: Virtual timer
 #define VIRTUAL_PIN_HOUR_INPUT V1
@@ -32,15 +32,19 @@ NTPClient timeClient(ntpUDP, "asia.pool.ntp.org", 7 * 3600, 60000);
 //prevents spamming the Blynk.Cloud with thousands of messages from your hardware
 BlynkTimer timer;
 
-//Set up pin to communicate with Arduino by UART
-#define RX_PIN 13 //D7
-#define TX_PIN 15 //D8
-SoftwareSerial arduinoSerial(RX_PIN, TX_PIN);
+// //Set up pin to communicate with Arduino by UART
+// #define RX_PIN D5 //D7  
+// #define TX_PIN D6 //D8
+// SoftwareSerial arduinoSerial(RX_PIN, TX_PIN);
+
+// Địa chỉ I2C của LCD và Arduino
+#define LCD_I2C_ADDRESS 0x27  // Địa chỉ I2C của LCD
+#define ARDUINO_I2C_ADDRESS 8  // Địa chỉ I2C của Arduino
 
 // Declare LCD (I2C address, columns, rows)
-LiquidCrystal_I2C lcd(0x27, 16, 2);
-#define SDA_PIN 4//D2
-#define SCL_PIN 5//D1
+LiquidCrystal_I2C lcd(LCD_I2C_ADDRESS, 16, 2);
+#define SDA_PIN D2//D2
+#define SCL_PIN D1//D1
 
 //define variable
 float weightLevelVal;
@@ -50,34 +54,9 @@ String message;
 int feedingHour = 0;
 int feedingMinute = 0;
 // variable to store the current time
-int currentHour = 1;
-int currentMinute = 1;
-
-//-------------------SET UP-------------------------------------------------
-
-void setup() {
-  //debug console
-  Serial.begin(9600);
-  //set up to communicate with ESP
-  arduinoSerial.begin(9600);
-
-  //set up Blynk
-  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
-
-  // Initialize NTP Client
-  timeClient.begin();
-
-  //set up a function to be called at any time
-  //  5000 milliseconds (5 seconds), and the letter L is used to specify that this value is a long integer
-  timer.setInterval(5000L , myTimerEvent);
-
-  //set up lcd
-  Wire.begin(SDA_PIN, SCL_PIN); //set up I2C pins
-  lcd.init();
-  lcd.backlight();
-  lcd.setCursor(0, 0);
-  lcd.print("Waiting for time");
-}
+int currentHour = 0;
+int currentMinute = 0;
+int currentSeconds = 0;
 
 //--------------------METHOD--------------------------------------
 
@@ -87,7 +66,7 @@ BLYNK_WRITE(VIRTUAL_PIN_HOUR_INPUT) {
   feedingHour = param.asInt();  // Get value from Slider 1 (hour)
   Serial.println("Time set: ");
   // Serial.println(feedingHour);
-  printSerialTime(feedingHour, feedingMinute);
+  printSerialTime(feedingHour, feedingMinute, 0);
 }
 
 // Event handler from Blynk: Get Minute input
@@ -96,26 +75,18 @@ BLYNK_WRITE(VIRTUAL_PIN_MINUTE_INPUT){
   feedingMinute = param.asInt();  // Get value from Slider 2 (minute)
   Serial.println("Time set: ");
   // Serial.println(feedingMinute);
-  printSerialTime(feedingHour, feedingMinute);
+  printSerialTime(feedingHour, feedingMinute, 0);
 }
 
-//Receive data from Arduino and send the weight value to Blynk
-void sendBlynkWeight(){
-  message = "";
-  Serial.println(message);
-  message = arduinoSerial.read();
-  weightLevelVal = message.toFloat(); 
-  Serial.println(weightLevelVal);
-  Blynk.virtualWrite(VIRTUAL_PIN_WEIGHT_OUTPUT, weightLevelVal);
-}
 
 // Get current time through Blynk
 void getRealTime(){
   timeClient.update();
   currentHour = timeClient.getHours();
   currentMinute = timeClient.getMinutes();
+  currentSeconds = timeClient.getSeconds();
   Serial.println("Real time: ");
-  printSerialTime(currentHour, currentMinute);
+  printSerialTime(currentHour, currentMinute, currentSeconds);
 }
 
 // Display the time on LCD
@@ -132,27 +103,80 @@ void displayTimeSet(){
     lcd.print(feedingMinute);
 }
 
-void printSerialTime(int hour, int minute){
+void printSerialTime(int hour, int minute, int seconds){
   Serial.print(hour);
   Serial.print(":");
-  Serial.println(minute);
+  Serial.print(minute);
+  Serial.print(":");
+  Serial.println(seconds);
 }
 
 bool checkTimeSet(){
   bool isTimeReached = false;
-  if(currentHour == feedingHour && currentMinute == feedingMinute)
+  if((currentHour == feedingHour) && (currentMinute == feedingMinute) && (currentSeconds = 0))
   {
     isTimeReached = true;
   }
   return isTimeReached;
 }
 
+//Receive data from Arduino and send the weight value to Blynk
+void receiveWeightData() {
+  Wire.requestFrom(ARDUINO_I2C_ADDRESS, sizeof(weightLevelVal));
+  if (Wire.available()) {
+    byte buffer[sizeof(weightLevelVal)];
+    for (int i = 0; i < sizeof(weightLevelVal); i++) {
+      buffer[i] = Wire.read();
+    }
+    memcpy(&weightLevelVal, buffer, sizeof(weightLevelVal));
+    Serial.print("Received weight: ");
+    Serial.println(weightLevelVal);
+    Blynk.virtualWrite(VIRTUAL_PIN_WEIGHT_OUTPUT, weightLevelVal);  // Gửi dữ liệu đến Blynk
+  }
+}
+
+void sendFeedingSignal() {
+  Wire.beginTransmission(ARDUINO_I2C_ADDRESS);
+  byte signal = (checkTimeSet()) ? 1 : 0;
+  Wire.write(signal);
+  Wire.endTransmission();
+}
+
 //call method after a time period based on timer.setInterval, ignore loop()
 void myTimerEvent(){
   getRealTime();
-  sendBlynkWeight();
+  // receiveBlynkWeight();
+  receiveWeightData();
+  sendFeedingSignal();
   displayTimeSet();
 }
+
+//-------------------SET UP-------------------------------------------------
+
+void setup() {
+  //debug console
+  Serial.begin(9600);
+  //set up to communicate with ESP
+  // arduinoSerial.begin(9600);
+    //set up lcd
+  Wire.begin(SDA_PIN, SCL_PIN); //set up I2C pins
+
+  //set up Blynk
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
+
+  // Initialize NTP Client
+  timeClient.begin();
+
+  //set up a function to be called at any time
+  //  5000 milliseconds (5 seconds), and the letter L is used to specify that this value is a long integer
+  timer.setInterval(5000L , myTimerEvent);
+
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("Waiting for time");
+}
+
 //------------------------LOOP----------------------------------------------
 
 void loop() {
@@ -161,12 +185,14 @@ void loop() {
   //run timer to send data periodically
   timer.run();
 
-  //send signal to feed
+
+  // sendBlynkWeight();
+  // send signal to feed
   if (checkTimeSet()) {
     Serial.println("Feeding the pet...");
     lcd.clear();
     lcd.setCursor(0, 0);
     lcd.print("Feeding now!");
-    arduinoSerial.println("FEED");
   }
+  // delay(800);
 }
